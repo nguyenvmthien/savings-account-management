f79c4f2f6bf899c0a742f15eb8479c55058e6611
04nhatminh <04nhatminh@gmail.com>
2024-08-05 16:39:14 +0700

    Minh: merge

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const CreateHomeRouter = require("./home");
 const CreateStartingRouter = require ("./starting");
+const createRouter = require("./create")
 function route(app) {
-    app.use ("/starting/home", CreateHomeRouter);
+    app.use("/starting/home", CreateHomeRouter);
     app.use("/starting", CreateStartingRouter);
-    app.use('/sa-create', createRouter);
+    app.use("/sa-create", createRouter);
 }
 
 module.exports = route;
