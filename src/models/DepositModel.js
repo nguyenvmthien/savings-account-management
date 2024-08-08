3f7152e00114f28a7de13bcf8de0105eb32574e9
AnhCao <anhnguyencao2712@gmail.com>
2024-08-08 15:57:00 +0700

    "Anh: update DepositModel"

--- src/models/DepositModel.js
+++ src/models/DepositModel.js
@@ -6,6 +6,66 @@ class Deposit_H {
             // Insert a new deposit into the database
             // Update into balance table
             // Return the deposit information
+            const connection = await pool.getConnection();
+            await connection.beginTransaction();
+            try{
+                // Check if the account already exists
+                const [existingAccount] = await connection.execute(
+                    'SELECT * FROM account WHERE acc_id = ?',
+                    [id_account]
+                );  
+
+                if(existingAccount.length > 0)
+                {
+                    const[account] = await connection.execute(
+                        `
+                        SELECT a.open_date
+                        FROM account a
+                        WHERE a.acc_id = ? 
+                        ORDER BY a.open_date DESC
+                        LIMIT 1
+                        `, [id_account]
+                    );
+
+                    const p_open_date = account[0].open_date;
+                        if(money_deposit > 500000 && deposit_date > p_open_date)
+                        {
+                            await connection.execute(
+                                `SET @p_dep_id = CONCAT('DEP', LPAD(FLOOR(RAND() * 100000), 5, '0'))`
+                            )
+
+                            await connection.execute(
+                                'INSERT INTO deposit (dep_id, acc_id, dep_money, dep_date) VALUES (@p_dep_id, ?, ?, ?)',
+                                [id_account, money_deposit, deposit_date]
+                            )
+
+                            const[balance] = await connection.execute(
+                                `
+                                SELECT a.cur_balance
+                                FROM balance b
+                                WHERE b.acc_id = ? 
+                                ORDER BY b.open_date DESC
+                                LIMIT 1
+                                `, [id_account]
+                            );
+        
+                            const p_cur_balance = balance[0].cur_balance + money_deposit;
+
+                            await connection.execute(
+                                'UPDATE balance SET cur_balance = ? WHERE acc_id = ?', [p_cur_balance, id_account]
+                            )
+                            
+                            await connection.commit();
+                            
+                        }
+                    }
+                }catch (err) {
+                    await connection.rollback();
+                    console.error('Error during transaction:', err);
+                    throw err;
+                } finally {
+                    connection.release();
+                }
         } catch {
             console.error('Error deposit:', err);
             throw err;
