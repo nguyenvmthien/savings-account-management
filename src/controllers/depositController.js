e440f253abfb8f29fb69ac08219f34b86df91927
04nhatminh <04nhatminh@gmail.com>
2024-08-20 14:00:55 +0700

    Minh: Push to pull

--- src/controllers/depositController.js
+++ src/controllers/depositController.js
@@ -16,12 +16,11 @@ class Deposit_Controller {
             return;
         }
         try {
-            // const result = await depositModel.deposit({
-            //     id_account,
-            //     money_deposit,
-            //     deposit_date,
-            // });
-            // res.json(result);
+            await depositModel.deposit({
+                id_account,
+                money_deposit,
+                deposit_date,
+            });
             res.json({message: "success" });
         } catch (error) {
             res.json({ message: "fail" });
