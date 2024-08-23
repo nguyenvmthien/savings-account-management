3d9e023b2d8879c2f645e7eeeab869c9424f2881
nguyenvmthien <nguyenvmthien@gmail.com>
2024-08-23 21:25:00 +0700

    Thien: handle display in findAccount page

--- src/routes/find.js
+++ src/routes/find.js
@@ -4,7 +4,7 @@ const findAccountController = require('../controllers/findAccountController');
 const apiController = require('../controllers/apiController');
 
 router.get('/createReportAPI', apiController.findAccountAPI);
-router.get('/getNewestAccountAPI', apiController.getNewestIDAccountAPI);
+router.get('/getNewestAccountAPI', apiController.getLatestAccountsAPI);
 router.get('/getTypeAPI', apiController.getAllTypeOfSavingAPI);
 router.get('/', findAccountController.renderSaFindAccount);
 
