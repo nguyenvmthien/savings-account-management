fc0aa292a686f55f7a60b7c648d8f5b53a0e70a3
nguyenvmthien <nguyenvmthien@gmail.com>
2024-08-23 15:21:41 +0700

    Thien: Update Controller, Model

--- src/models/AccountModel.js
+++ src/models/AccountModel.js
@@ -49,7 +49,7 @@ class Account_H {
                 // Get the apply_date from the regulation table based on date_created
                 const [regulation] = await connection.execute(
                     `
-                    SELECT r.apply_date 
+                    SELECT r.apply_date, r.interest_rate
                     FROM regulation r 
                     WHERE r.type = ? AND r.apply_date <= ? 
                     ORDER BY r.apply_date DESC 
@@ -239,17 +239,70 @@ class Account_H {
 
     // OK
     async getNewestIDAccount() {
+        // If temp is empty -> update temp by newest id in account + 1, Update status empty -> in-use and return newest_id in temp.
+        // If temp is not empty
+        // If exist empty id in temp -> return empty smallest id in temp
+        // Else
+        // -> add new_id in temp by newest current id in account + 1
+        // -> return newest_id in temp. Update status empty -> in-use
+
         try {
-            const query = `
-                SELECT MAX(acc_id) AS newest_id_account
-                FROM account
-                WHERE acc_id LIKE 'MS%' AND LENGTH(acc_id) = 12;
+            // get id in temp_id_account has in_use status = 0
+            const sql = `
+                SELECT id_account
+                FROM temp_id_account
+                WHERE in_use = 0;
             `;
 
-            const [rows, fields] = await pool.execute(query);
-            console.log(rows);
-            return rows[0].newest_id_account;
-            // get newest id_account
+            const [rows1, fields1] = await pool.execute(sql);
+            if (rows1.length > 0) {
+                // update status in_use
+                const sql1 = `
+                    UPDATE temp_id_account
+                    SET in_use = 1
+                    WHERE id_account = ?;
+                `;
+
+                await pool.execute(sql1, [
+                    rows1[0].id_account,
+                ]);
+                return rows1[0].id_account;
+            } else {
+                // get newest id_account
+                const query1 = `SELECT MAX(acc_id) AS newest_id_account FROM account WHERE acc_id LIKE 'MS%' AND LENGTH(acc_id) = 12;`;
+                const [rows2, fields2] = await pool.execute(query1);
+
+                const query2 = `SELECT MAX(id_account) AS newest_id_account FROM temp_id_account;`;
+                const [rows3, fields3] = await pool.execute(query2);
+
+                var pre_id_account = rows2[0].newest_id_account;
+                if (rows3.length !== 0) {
+                    // compare newest id_account in account and newest id_account in temp_id_account
+                    const temp1 = rows2[0].newest_id_account;
+                    const temp2 = rows3[0].newest_id_account;
+
+                    if (temp2 > temp1) {
+                        pre_id_account = temp2;
+                    }
+                } 
+                // Split id_account into prefix and number
+                const prefix = 'MS';
+                const number = pre_id_account.slice(prefix.length); // Slice after the prefix
+                // Increase id_account
+                const next_number = (parseInt(number, 10) + 1)
+                    .toString()
+                    .padStart(number.length, '0');
+                const id_account = prefix + next_number;
+
+                // add new_id in temp_id_account
+                const sql2 = `
+                    INSERT INTO temp_id_account (id_account, in_use)
+                    VALUES (?, 1);
+                `;
+
+                await pool.execute(sql2, [id_account]);
+                return id_account;
+            }
         } catch (err) {
             return { message: 'fail' };
             console.error('Error getting biggest id_account:', err);
