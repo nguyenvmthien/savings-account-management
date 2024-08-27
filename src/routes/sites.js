26be1d1f0c0b54589afa6b4297803fb985e9d064
04nhatminh <04nhatminh@gmail.com>
2024-08-28 03:36:26 +0700

    Minh: Update: Homescreen FE (chart)

--- src/routes/sites.js
+++ src/routes/sites.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const sitesController = require('../controllers/sitesController.js');
+const apiController = require('../controllers/apiController.js');
 
 router.get('/contact', sitesController.renderContact);
+router.get('/home/getChartData', apiController.getAllAndWithdrawTransactionAPI);
 router.get('/home', sitesController.renderHome);
 router.get('/starting', sitesController.renderStarting);
 router.get('/', sitesController.renderDefault);
